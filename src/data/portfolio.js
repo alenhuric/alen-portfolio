20c7cfe47ea28e42fb98d42773ba09b936ac0195
hurke17 <hurica@mail.gvsu.edu>
2023-08-28 15:49:53 -0400

    Cleaned up img src look.

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,26 +1,32 @@
 export default[
     {
         title: 'This Site',
-        imgUrl: '/assets/reactJS.png',
+        imgUrl: '/assets/react.png',
         stack: ['React', 'Tailwind CSS', 'Vite'],
         link: 'https://github.com/alenhuric/alen-portfolio'
     },
     {
         title: 'Bicycle Challenge (MySQL)',
-        imgUrl: '/assets/Symfony_Welcome_Page.png',
+        imgUrl: '/assets/Symfony.jpeg',
         stack: ['Symfony', 'Twig', 'MySQL'],
         link: 'https://github.com/alenhuric/Bike_Challenge_Alen_Huric'
     },
     {
         title: 'Bicycle Challenge (MongoDB)',
-        imgUrl: '/assets/Symfony_Welcome_Page.png',
+        imgUrl: '/assets/mongodb.jpeg',
         stack: ['Symfony', 'Twig', 'MongoDB'],
         link: 'https://github.com/alenhuric/BicycleChallenge2/tree/master'
     },
     {
         title: 'To-Do List',
-        imgUrl: '/assets/to-do-app.png',
+        imgUrl: '/assets/nodejs.jpeg',
         stack: ['React', 'Node.js', 'Tailwind CSS'],
         link: 'https://github.com/alenhuric/To-Do-List-App'
+    },
+    {
+        title: 'Task List',
+        imgUrl: '/assets/laravel.png',
+        stack: ['Laravel', 'Eloquent', 'Tailwind CSS'],
+        link: 'https://github.com/alenhuric/Laravel-Task-List-App/tree/main'
     },
 ]
