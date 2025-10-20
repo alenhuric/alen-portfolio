fa70a491055363fae8ca7c9dbdc62bc443a6f06a
hurke17 <hurica@mail.gvsu.edu>
2025-10-20 12:14:02 -0400

    Timeline change.

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,32 +1,16 @@
 export default [
-    {
-        year: '2024',
-        title: 'Full Stack Developer',
-        location: 'CESA 6',
-        details: 'Modernized backend systems with Laravel, improving API data flow, designing database models, and developing web apps with Vue, TypeScript, TailwindCSS, and Vuetify.'
-    },
-    {
-        year: '2022',
-        title: 'PHP Developer',
-        location: 'Professional Computing Resources',
-        details: 'Leveraged Zend Framework for the design and development of web applications.'
-    },
-    {
-        year: '2020',
-        title: 'Software Developer',
-        location: 'YRC Worldwide',
-        details: "Designed, implemented, and maintained applications using Natural programming language and Software AG's Adabas database management system."
-    },
-    {
-        year: '2019',
-        title: 'Software Development Intern',
-        location: 'YRC Worldwide',
-        details: 'Contributed to the testing and development of Adabas business applications using Natural.'
-    },
-    {
-        year: '2016',
-        title: 'Computer Science, B.S',
-        location: 'Grand Valley State University',
-        details: 'Completed coursework in various subjects, including Databases, System-Level Programming, Data Structures & Algorithms, Microarchitecture & Assembly, Game Design and Cryptography.'
-    },
-]
+  {
+    year: "2024",
+    title: "Full Stack Developer",
+    location: "CESA 6",
+    details:
+      "Modernized backend systems with Laravel, improving API data flow, designing database models, and developing web apps with Vue, TypeScript, TailwindCSS, and Vuetify.",
+  },
+  {
+    year: "2022",
+    title: "PHP Developer",
+    location: "Professional Computing Resources",
+    details:
+      "Leveraged Zend Framework for the design and development of web applications.",
+  },
+];
